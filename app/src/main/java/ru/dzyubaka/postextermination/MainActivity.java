66ab4e88a736964454ec36732e15d9379bf02ea9
Konstantin Dzyuba <konstantin.dzyuba.2003@gmail.com>
2024-12-23 17:41:33 +0300

    day one

--- app/src/main/java/ru/dzyubaka/postextermination/MainActivity.java
+++ app/src/main/java/ru/dzyubaka/postextermination/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int sanity, hunger, thirst, sleep, toxins, pain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +22,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new MapFragment()).commit();
     }
 }
---
+++ app/src/main/java/ru/dzyubaka/postextermination/MapFragment.java
@@ -0,0 +1,18 @@
+package ru.dzyubaka.postextermination;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MapFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+}
